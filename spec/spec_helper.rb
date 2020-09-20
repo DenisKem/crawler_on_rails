9353eb0faa0095ab79095f9df0864b6d29b24f5e
Denis <denis.kem@gmail.com>
2020-09-21 01:07:13 +0300

    Parser empty

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -97,4 +97,6 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+  Dir[Rails.root.join('app/services', '**', '*.rb')].each {|f| require f}
 end
